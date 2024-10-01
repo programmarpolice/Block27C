34fcb14a1b0342099fe4f45223ad4124b1e68c15
Joy Harrington <joysyk23@gmail.com>
2024-10-01 18:50:34 -0500

    matched solution exactly, still not working.

--- src/store/api.js
+++ src/store/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const COHORT_CODE = "2408-Joy";
-const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${COHORT_CODE}/players`;
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${COHORT_CODE}/`;
 // the only way I can get anything to show up was if I added players to the end of the API URL
 // but then it breaks everything else
 
